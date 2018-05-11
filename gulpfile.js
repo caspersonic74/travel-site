bf54b62074257d973ee316783dbc754f830d21af
Donny <donny@buttercups.nl>
2018-05-11 11:39:46 +0200

    Completed large hero block

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
+
+var gulp = require('gulp-help')(require('gulp'));
+
+gulp.task('lint', 'Lints all server side js', function () {
+    gulp.src('./lib/**/*.js')
+      .pipe(jshint());
+});
