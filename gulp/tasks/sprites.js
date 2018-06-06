d8d2e35d376b43044ae1042c18353658ecfc3de6
Donny <donny@buttercups.nl>
2018-06-06 06:05:56 +0200

    navigation look version 1.0

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -9,35 +9,35 @@ var config = {
       sprite: 'sprite.svg',
       render: {
         css: {
-          template: './gulp/templates/sprite.css',
-        },
-      },
-    },
-  },
-};
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+}
 
-gulp.task('beginClean', function () {
+gulp.task('beginClean', function() {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite', ['beginClean'], function () {
+gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function () {
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/**/*.svg')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
-gulp.task('copySpriteCSS', ['createSprite'], function () {
+gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
   return del('./app/temp/sprite');
 });
 
